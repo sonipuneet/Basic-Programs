b5ac646405b07a8b5eb224ad2b49756a0f16daeb
Puneet Soni <sonipuneet@users.noreply.github.com>
2019-10-17 19:31:15 -0700

    Create non_decreasing_array.cpp

---
+++ non_decreasing_array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Solution {
+public:
+    bool checkPossibility(int nums[]) {
+        int length = nums.length(); 
+        int count = 0; 
+
+    }
+};
+
+
+int main(){
+
+Solution solution;
+solution.checkPossibility();
+
+cout << "Output is :" <<  ; 
+
+return 0;
+
+}
