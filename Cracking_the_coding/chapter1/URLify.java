93760bfc20891fac354a625077d59a89037775c2
Puneet Soni <puneetsoni.mail@gmail.com>
2020-01-20 20:18:28 -0800

    Updated URLify.java

--- Cracking_the_coding/chapter1/URLify.java
+++ Cracking_the_coding/chapter1/URLify.java
@@ -2,48 +2,46 @@ package chapter1;
 
 public class URLify {
 
-	void replaceSpaces(String s1, int string_length) {
-		char[] ch1 = s1.toCharArray();
+    void replaceSpaces(String s1, int string_length) {
+        char[] ch1 = s1.toCharArray();
 
-		// To count the spaces in a given string
-		int space_count = 0;
+        // To count the spaces in a given string
+        int space_count = 0;
 
-		for (int i = 0; i < s1.length(); i++) {
-			if (ch1[i] == ' ') {
-				space_count++;
-			}
+        for (int i = 0; i < s1.length(); i++) {
+            if (ch1[i] == ' ') {
+                space_count++;
+            }
 
-			int new_length = string_length + space_count * 2;
+            int new_length = string_length + space_count * 2;
 
-			
-		}
 
-	}
+        }
 
-	}
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		String s1 = "This is a test";
+        String s1 = "This is a test";
 
-		int string_length = s1.length();
+        int string_length = s1.length();
 
-		char[] c1 = s1.toCharArray();
+        char[] c1 = s1.toCharArray();
 
-		for (int i = 0; i < s1.length(); i++) {
+        for (int i = 0; i < s1.length(); i++) {
 
-			int count_space = 0;
+            int count_space = 0;
 
-			if (c1[i] == " ") {
+            //if (c1[i] == " ") {
 
-			}
+            //}
 
-		}
+        }
 
-		System.out.println("Initial String output:" + s1);
+        System.out.println("Initial String output:" + s1);
 
-		System.out.println("Final String output:" + s1);
+        System.out.println("Final String output:" + s1);
 
-	}
+    }
 
 }
